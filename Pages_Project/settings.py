6921baba9c3604e9474209a6f81ccc032da0acc0
jarvis <justin.needham@alumnimail.pepperdine.edu>
2018-04-28 15:50:18 -0600

    add new updates for Heroku deployments

--- Pages_Project/settings.py
+++ Pages_Project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'y5+5330gh+z#w=2m*%=cdq&o08^&pl4)e=^+-=)oaqnb1&&&=n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Pages_Project.wsgi --log-file -
